5fc9a2daef75dfbf037800d3ee07de11ac1e0ef6
Hukak_He_Pak <valsidalvonilb@outlook.com>
2022-02-17 20:27:17 +0400

    add auth and confirm service tabs, create request to mail api, fix time format

--- js/main.js
+++ js/main.js
@@ -6,16 +6,43 @@ const messageForm = UI.MESSAGE.CREATE_FORM;
 
 messageForm.addEventListener('submit', event => {
     const msgText = messageForm.elements.newMessage.value;
+
+    if(!msgText) {
+        event.preventDefault();
+        return;
+    }
+
     const msgNode = UI.MESSAGE.TEMPLATE.content.cloneNode(true);
 
     msgNode.querySelector('.content').textContent = msgText;
 
-    const date = new Date();
-
-    msgNode.querySelector('.time').textContent = date.getHours() + ':' + date.getMinutes();
+    msgNode.querySelector('.time').textContent = getTime();
 
     UI.MESSAGE.DISPLAY.append(msgNode);
 
     messageForm.reset();
     event.preventDefault();
 });
+
+
+
+function getTime() {
+    const date = new Date();
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    return (hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes;
+}
+
+// const request = await fetch('https://chat1-341409.oa.r.appspot.com/api/user', { 
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json;charset=utf-8'
+//     },
+//     body: JSON.stringify({
+//         email: 'nikak_ne_rak@mail.ru' 
+//     })
+// });
+
+// console.log(await request.json());
